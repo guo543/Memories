07b614e59d60f6033c73a7f8094802555004a542
guo543 <guo543@purdue.edu>
2022-09-16 16:40:45 -0400

    modified edit, delete logic

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,12 +2,13 @@ import jwt, { decode } from 'jsonwebtoken';
 
 const auth = async(req, res, next) => {
     try {
+        //console.log(req.headers);
         const token = req.headers.authorization.split(" ")[1];
         const isCustomAuth = token.length < 500;
 
         let decodedData;
-
         if (token && isCustomAuth) {
+            
             decodedData = jwt.verify(token, 'test');
 
             req.userId = decodedData?.id;
